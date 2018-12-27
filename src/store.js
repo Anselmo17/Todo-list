9cd575a091def04bfc5b41c2656853c26f6a599c
Anselmo <anselmo.dos.santos@avanade.com>
2018-12-27 09:41:29 -0200

    Created module initial

---
+++ src/store.js
@@ -0,0 +1,8 @@
+
+//CRIA O CREATE STORE  E PASSA UMA LISTA DE REDUCERS\
+import { createStore } from 'redux';
+
+const store = createStore(() => { });
+
+//EXPORTANDO O STORE(ARMAZENA O ESTADO DOS DADOS )
+export default store;
